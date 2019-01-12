6f8c73664283d5ea7f0765a9aad16f306c275b9c
OffShell System <46108318+OffShellSystem@users.noreply.github.com>
2019-01-12 16:24:57 +0100

    Update Wireless-Total-Firmware.sh

--- Wireless-Total-Firmware.sh
+++ Wireless-Total-Firmware.sh
@@ -1,29 +1,45 @@
 #!/bin/bash
-# //Programa creado por OffShell System. Versión 1.1
+
 # //Solución para posibles problemas con la tarjeta de red inalámbrica.
+# Version 1.2
+# Software OffShell System.
+
 clear
+echo -e "\e[0;36m                            <<<···*···>>>                       \e[0m"
+echo -e "\e[0;34m        <<<<<<<<<<<<<<<<<<<<.............>>>>>>>>>>>>>>>>>>>>>>>\e[0m"
 echo "                                                               "
-echo "                                                               "
-echo -e "\e[0;35m       00101  11010  01110  00110   11  01  00110  01    00\e[0m"
-echo -e "\e[0;35m       01 10  11     00     01      10  00  00     10    11\e[0m"
-echo -e "\e[0;35m       00 11  01101  11011  01101   100111  01010  00    10\e[0m"
-echo -e "\e[0;35m       01 00  11     10        10   11  01  01     11    00\e[0m"
-echo -e "\e[0;35m       00110  01     10     11011   10  01  11100  00110 10100\e[0m"
-echo "       ........................................................"
-echo "       ........................................................"
-echo -e "\e[0;35m       <<<<<<<<<<<<<<<<<<<<.............>>>>>>>>>>>>>>>>>>>>>>>\e[0m"
+echo -e "\e[1;35m  ?¿¿?¿¿?¿?¿?¿?¿?¿?¿?\e[0;35m¿?¿?¿?¿?¿?¿?¿?¿?¿?¿?¿?¿?¿?¿\e[1;35m?¿?¿?¿?¿??¿?¿?¿?¿?¿ \e[0m"
+echo -e "\e[0;35m  ¿? \e[0m \e[1;35m 00101  11010  01110  00110   11  01  00110  01    00  \e[0m  \e[0;35m  ?¿ \e[0m"
+echo -e "\e[1;35m  ?¿ \e[0m \e[0;35m 01 10  11     00     01      10  00  00     10    11  \e[0m  \e[1;35m  ?¿ \e[0m"
+echo -e "\e[0;35m  ¿? \e[0m \e[1;35m 00 11  01101  11011  01101   100111  01010  00    10  \e[0m  \e[0;35m  ?¿ \e[0m"
+echo -e "\e[1;35m  ?¿ \e[0m \e[0;35m 01 00  11     10        10   11  01  01     11    00  \e[0m  \e[1;35m  ?¿ \e[0m"
+echo -e "\e[0;35m  ¿? \e[0m \e[1;35m 00110  01     10     11011   10  01  11100  00110 10100 \e[0m \e[0;35m ?¿ \e[0m"
+echo -e "\e[1;35m  ?¿¿?¿?¿?¿?¿?¿?¿?¿?¿\e[0;35m?¿?¿?¿?¿?¿?¿?¿?¿?¿?¿?¿?¿?¿?\e[1;35m¿?¿?¿?¿?¿?¿?¿?¿?¿?¿ \e[0m"
+echo ""
+echo -e "\e[0;34m        <<<<<<<<<<<<<<<<<<<<.............>>>>>>>>>>>>>>>>>>>>>>>\e[0m"
+echo -e "\e[0;36m                            <<<···*···>>>                       \e[0m"
 sleep 3
 echo "                                                               "
 echo "       <·OffShell System Underground. Script bash for shell·>"
 echo "                                                               "
+echo -e "\e[1;35m           	<·WIRELESS-TOTAL-FIRMWARE <-> V-1.0·>\e[0m"
 echo "                                                               "
 echo "       Bienvenido al sistema de automatización para DEBIAN OS."
-echo "                            <<<···>>>"
 echo "                                                              "
 echo "          "
 echo "          "
 echo "          "
-echo "Presiona cualquier tecla para continuar..."
+echo -e "\e[0;36m Programa registrado con licencia GPL V-3 [Copyleft] \e[0m"
+			echo "-----------------------------------------------------------"
+			echo ""
+			echo -e "\e[0;36m           >>> OFFSHELL SYSTEM © \e[0m"
+			echo ""
+			echo "                                                    -Feel the system-"
+			echo "                                                         <<<*>>>"
+			echo "                                                           <*>"
+			echo "                                                            -"
+echo ""
+echo -e "		<<< Presiona cualquier tecla para continuar >>>"
 read foo
 
 # //Menú principal.
@@ -64,10 +80,33 @@ do
 		   sleep 1
 		   echo "Ejecutando solución...: ";
 		   sleep 1
-		   apt-get update && apt-get remove bcmwl-kernel-source && apt-get install firmware-b43-installer && reboot ;;
+		   apt-get update && apt-get remove bcmwl-kernel-source && apt-get install firmware-b43-installer && reboot ;
+		   echo -e "\e[0;34m	<<< Pulse cualquier tecla para acceder al menú del programa >>>\e[0m" ;
+           read foo ;;
 
 		2) echo "Saliendo de OffShell System...: ";
-		sleep 3
+		   echo "Saliendo del programa: ";
+			sleep 2;
+			echo "";
+			echo -e "\e[0;34m >>>CERRANDO SESIÓN>>>... \e[0m";
+			sleep 1
+			echo "";
+			echo -e "\e[0;34m >>>GRACIAS POR USAR OFFSHELL SYSTEM>>>... \e[0m";
+			sleep 1
+			echo "";
+			echo "-----------------------------------------------------------";
+			echo -e "\e[0;36m Programa registrado con licencia GPL [Copyleft] \e[0m";
+			echo "-----------------------------------------------------------";
+			echo "";
+			echo -e "\e[0;34m           >>>OFFSHELL SYSTEM © \e[0m";
+			echo "";
+			echo "                                                    -Feel the system-";
+			echo "                                                         <<<*>>>";
+			echo "                                                           <*>";
+			echo "                                                            -";
+			echo "";
+			echo -e "\e[0;34m	<<< Pulse cualquier tecla para salir del programa. >>>\e[0m" ;
+			read foo;
 		exit 1;;
 
 		*) echo "$opc La opción pulsada no existe: ";
